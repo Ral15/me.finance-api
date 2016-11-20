8778e3614da57489fcf090f1a09bb36339344086
Raul Mar <Ral15@Rauls-MacBook-Pro.local>
2016-11-19 20:02:08 -0600

    create user and get user by id

--- hello.py
+++ hello.py
@@ -1,4 +1,6 @@
 import os
+import re
+from flask_bcrypt import Bcrypt
 from flask import Flask, request
 from flask_restful import Resource, Api, reqparse, fields, marshal_with
 from flask_sqlalchemy import SQLAlchemy
@@ -10,6 +12,8 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 app = Flask(__name__)
 api = Api(app)
+bcrypt = Bcrypt(app)
+
 app.config['SQLALCHEMY_DATABASE_URI'] =\
     'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 app.config['SQLALCHEMY_COMMIT_ON_TEARDOWN'] = True
@@ -28,17 +32,18 @@ manager.add_command('db', MigrateCommand)
 class User(db.Model):
 	__tablename__ = 'users'
 	id = db.Column(db.Integer, primary_key=True, unique=True)
-	username = db.Column(db.String(64), unique=True, index=True)
+	# username = db.Column(db.String(64), unique=True, index=True)
 	first_name = db.Column(db.String(64))
 	last_name = db.Column(db.String(64))
 	password = db.Column(db.String(64))
+	email = db.Column(db.String(64), unique=True)
 	bills = db.relationship('Bill')
 	incomes = db.relationship('Income', backref='user', lazy='dynamic')
 	payments = db.relationship('Payment', backref='user', lazy='dynamic')
 	is_premium = db.Column(db.Boolean(), default=True)
 
 	def __repr__(self):
-		return '<User %r>' % self.username
+		return '<User %r>' % self.email
 
 class Bill(db.Model):
 	__tablename__ = 'bills'
@@ -105,30 +110,71 @@ class Tip(db.Model):
 
 parser = reqparse.RequestParser()
 
-class Usr(Resource):
+
+###########
+#	User  #
+###########
+
+#Validation functions
+
+def validEmail(email):
+	if not re.match(r"[^@]+@[^@]+\.[^@]+", email):
+		raise ValueError("The email: '{}' is not valid.".format(email))
+	else:
+		return email
+
+def validPassword(password):
+	if len(password) >= 8:
+		return bcrypt.generate_password_hash(password)
+	else:
+		raise ValueError("The password mus be at least 8 characters long.")
+
+
+class UserStore(Resource):
+	#Get all users
 	user_fields = {
     	'id': fields.String,
-    	'username':   fields.String,
+    	'email':   fields.String,
+    	# 'password': fields.String,
+    	'first_name': fields.String,
+    	'last_name': fields.String
 	}
-	@marshal_with(user_fields)
-	def get(self):
-		return User.query.all()
 
+	# """Get all users"""
+	# @marshal_with(user_fields)
+	# def get(self):
+	# 	return User.query.all()
+
+	"""Get user by id"""
+	@marshal_with(user_fields)
+	def get(self, id):
+		user = User.query.filter(User.id == id).first()
+		if not user:
+			abort(404, message="User {} doesn't exist".format(id))
+		return user
+
+	"""Create user"""
+	@marshal_with(user_fields)
 	def post(self):
-		parser.add_argument('username', type=str, required=True)
-		parser.add_argument('first_name', type=str, required=True)
-		parser.add_argument('last_name', type=str, required=True)
-		parser.add_argument('password', type=str, required=True)
+		# parser.add_argument('username', type=str, required=True)
+		parser.add_argument('first_name', type=str, required=True, help="First name cannot be blank!")
+		parser.add_argument('last_name', type=str, required=True, help="Last name cannot be blank!")
+		parser.add_argument('password', type=validPassword, required=True)
+		parser.add_argument('email', type=validEmail, required=True)
 		args = parser.parse_args()
-		new_user = User(username=args['username'], 
-						first_name=args['first_name'],
+		new_user = User(first_name=args['first_name'],
 						last_name=args['last_name'],
+						email=args['email'],
 						password=args['password']) 
 		db.session.add(new_user)
 		db.session.commit()
-		return '' , 201
+		return new_user, 201
 
-api.add_resource(Usr,	 '/auth/create', '/auth/all')
+api.add_resource(UserStore,	 '/auth/create', '/auth/<int:id>')
 
-# if __name__=='__main__':
-# 	app.run(debug=True)
+############
+#	Bill   #
+############
+
+if __name__=='__main__':
+	app.run(debug=True)
